6696701b087baf0da79ab2fdf5b0222ef992ab08
Felipe M. Vieira <felipev@telnyx.com>
2022-07-24 14:38:27 -0300

    added `MetaData` (empty) model

---
+++ code/alembic/model/fmv1992_backup_system_database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import (
+    Boolean,
+    Column,
+    DateTime,
+    MetaData,
+    String,
+    Table,
+    text,
+    Text,
+    UniqueConstraint,
+)
+# from sqlalchemy.dialects.postgresql import JSONB
+
+metadata = MetaData()
