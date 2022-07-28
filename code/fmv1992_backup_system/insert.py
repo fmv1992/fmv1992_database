bff324045827cf6552dba72c14195dfae533b9b8
Felipe M. Vieira <felipev@telnyx.com>
2022-07-28 08:56:31 -0300

    added comments

--- code/fmv1992_backup_system/insert.py
+++ code/fmv1992_backup_system/insert.py
@@ -4,6 +4,12 @@ import sys
 def main():
     # Read stdin in chunks (tar format).
     # Iterate over files in chunks ().
+    #
+    # I'm using tar due to it:
+    #
+    # 1.    Informing the path of the byte stream.
+    #
+    # 1.    Provide the bytestream.
     pass
 
 if __name__ == '__main__':
